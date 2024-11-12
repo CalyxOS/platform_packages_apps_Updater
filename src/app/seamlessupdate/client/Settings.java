6db4e6ab9e284eef9c49b652f4051f84820c7bef
Chirayu Desai <chirayudesai1@gmail.com>
2024-11-12 23:20:36 +0530

    Revert "stable7, beta7 and security-express7"
    This reverts commit 98f68f809268d60a4cc5cf7c386535ca40fd5489.
    
    Reason: This shipped in 5.12.0 for pixels 6 - 8a.
    Now this branch (14) will be used for other devices 14 builds.
    We'll bring it back as and when needed for 14 -> 15 upgrades,
    as they're figured out for the various devices
    
    Change-Id: I50eaba0993b65cc2a1d718f45de8d3b10cfec1eb

--- src/app/seamlessupdate/client/Settings.java
+++ src/app/seamlessupdate/client/Settings.java
@@ -30,9 +30,9 @@ public class Settings extends PreferenceActivity {
     }
 
     static String migrateChannel(final String prefChannel) {
-        if (prefChannel.startsWith("security-express")) return "security-express7";
-        else if (prefChannel.startsWith("stable")) return "stable7";
-        else if (prefChannel.startsWith("beta")) return "beta7";
+        if (prefChannel.startsWith("security-express")) return "security-express6";
+        else if (prefChannel.startsWith("stable")) return "stable6";
+        else if (prefChannel.startsWith("beta")) return "beta6";
         else return prefChannel;
     }
 
