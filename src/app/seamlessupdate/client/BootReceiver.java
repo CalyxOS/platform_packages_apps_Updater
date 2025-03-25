ca769fe1dd22463b3b3004c0080084281e1a09df
Tommy Webb <tommy@calyxinstitute.org>
2025-03-25 18:11:08 +0000

    End-of-support notice for all devices
    Display a final update notification for all devices,
    and show the same information on the update page in Settings.
    
    Co-authored-by: Sage Cheng <sagecheng@calyxinstitute.org>
    Change-Id: I2ebea2217ea01e19b2ba7b1ee3aa9c3fc917f651

--- src/app/seamlessupdate/client/BootReceiver.java
+++ src/app/seamlessupdate/client/BootReceiver.java
@@ -5,11 +5,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.UserManager;
+import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
+    private final String TAG = BootReceiver.class.getSimpleName();
+
     @Override
     public void onReceive(final Context context, final Intent intent) {
         if (context.getSystemService(UserManager.class).isSystemUser()) {
+            try {
+                new NotificationHandler(context).showFinalUpdateNotification();
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to display final update notification", e);
+            }
             Settings.getPreferences(context).edit().putBoolean(Settings.KEY_WAITING_FOR_REBOOT, false).apply();
             PeriodicJob.schedule(context);
         } else {
