6df68a283aa27e90422a80b00daef6adb32f01a3
Tommy Webb <tommy@calyxinstitute.org>
2025-05-27 17:27:02 -0400

    Updater is legacy; maybe notify about settings
    Add a notice to the updater settings about its legacy status.
    
    If the user has changed any of the updater settings from their defaults,
    notify the user to check the settings of the new updater.
    
    Based on "End-of-support notice for Pixel 3 and 4"
    (I2ebea2217ea01e19b2ba7b1ee3aa9c3fc917f651) and its fixup.
    
    Issue: calyxos#3257
    Issue: calyxos#3220
    Change-Id: Ifca24e84374138634e109ec82387d64810971fdc

--- src/app/seamlessupdate/client/BootReceiver.java
+++ src/app/seamlessupdate/client/BootReceiver.java
@@ -5,11 +5,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.UserManager;
+import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
+    private final String TAG = BootReceiver.class.getSimpleName();
+
     @Override
     public void onReceive(final Context context, final Intent intent) {
         if (context.getSystemService(UserManager.class).isSystemUser()) {
+            try {
+                if (Settings.shouldShowNewUpdaterNotification(context)) {
+                    new NotificationHandler(context).showNewUpdaterNotification();
+                }
+
+                // Mark the notification as shown even if we did not need to show it, because
+                // users who didn't change any of this before but who visit the settings page now
+                // will be informed at that point that these are legacy settings; we don't need
+                // to tell them on the next boot if the info was already presented to them.
+                Settings.getPreferences(context).edit()
+                        .putBoolean(Settings.KEY_LEGACY_UPDATER_NOTIFICATION_SHOWN, true)
+                        .apply();
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to display notification about new updater", e);
+            }
             Settings.getPreferences(context).edit().putBoolean(Settings.KEY_WAITING_FOR_REBOOT, false).apply();
             PeriodicJob.schedule(context);
         } else {
